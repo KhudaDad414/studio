21a76ea79cbe4282c7d1019e9d53cd7f7e171590
Khuda Dad Nomani <khudadad414@gmail.com>
2024-06-21 15:52:52 +0100

    add cache option

--- apps/studio-next/next.config.js
+++ apps/studio-next/next.config.js
@@ -36,6 +36,23 @@ const nextConfig = {
 
     return config
   },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Netlify-CDN-Cache-Control',
+            value: 'public, s-maxage=31536000, must-revalidate',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
